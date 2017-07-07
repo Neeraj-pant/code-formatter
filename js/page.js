c122626f8e2d017e7c1cbe9b1b2b420758570bb1
Neeraj Pant <neeraj.pant@nvish.com>
2017-07-07 18:08:43 +0530

    added ace editor

--- js/page.js
+++ js/page.js
@@ -2,14 +2,25 @@ var button = document.getElementById('submit');
 var lang = document.querySelectorAll("input[name=language]:checked")[0];
 var codearea = document.getElementById('code');
 
+var beautify = ace.require("ace/ext/beautify");
 var editor = ace.edit("editor");
-/*editor.setTheme("ace/theme/monokai");
-editor.getSession().setMode("ace/mode/javascript");*/
+editor.setTheme("ace/theme/monokai");
+editor.getSession().setMode("ace/mode/html");
+
+var radio_group = document.querySelectorAll("input[name=language]");
+
+for (var i = 0; i < radio_group.length; i++) {
+	radio_group[i].addEventListener('change', function(){
+		lang = this.value;
+		editor.getSession().setMode("ace/mode/"+lang);
+		//beautify.beautify(editor.session);
+	});
+}
+
 
 button.addEventListener('click', function(){
-	switch (lang.value){
-		case 'HTML':
-		console.log(codearea.value);
-		console.log(vkbeautify.xml(codearea.value));
-	}
+	var lang = document.querySelectorAll("input[name=language]:checked")[0];
+ 	editor.session.setMode("ace/mode/" + lang.value);
+	beautify.beautify(editor.session);
 });
+
